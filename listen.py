cb440b298b515a118c6ce34facd5dbfa470c135d
Abhishek <abhisheks@iitbhilai.ac.in>
2019-10-11 19:40:48 +0530

    all final

--- listen.py
+++ listen.py
@@ -1,18 +1,22 @@
 import socket
 import sys
+import json
+import os
 
 port = int(sys.argv[1])  # initiate port no above 1024
 server_socket = socket.socket() 
 server_socket.bind(('', port))
-file = open("chats.txt","w")
-file.close()
+os.system("rm -rf chats")
+os.system("mkdir chats")
 while True:
     server_socket.listen(1)
     conn, address = server_socket.accept()  # accept new connection
     data = conn.recv(1024).decode()
-    data="-->> "+data+"\n"
-    file = open('chats.txt','a') 
-    file.write(data) 
+    data = json.loads(data)
+    chatline=" -->> "+data["content"]+"\n"
+    print(data["content"])
+    file = open("chats/"+str(data["ip"]),'a') 
+    file.write(chatline) 
     file.close() 
     conn.close()  # close the connection
 
--- send.py
+++ send.py
@@ -1,19 +1,21 @@
 import socket
 import sys
+import json
 
-host = sys.argv[1]  # as both code is running on same pc
-port = int(sys.argv[2])  # socket server port number
-
+host = sys.argv[1]  # ip of reciver
+port = int(sys.argv[2])  # port 
+ip="127.0.0.1"
 client_socket = socket.socket()  # instantiate
 client_socket.connect((host, port))  # connect to the server
 
 message = raw_input("")  # take input
-file = open("chats.txt","a")
+file = open("chats/"+host ,"a")
 file.write("<<--"+message+"\n")
 file.close()
 
-
-client_socket.send(message.encode())  # send message
+data={"ip":ip,"content":message}
+data=json.dumps(data)
+client_socket.send(data.encode())  # send message
 
 
 client_socket.close()  # close the connection
